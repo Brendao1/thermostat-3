850a75dff8481d2544cc78e6e2dfd68272fb3fdb
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-07 16:38:56 +0000

    Added code to control buttons on interface

--- controller.js
+++ controller.js
@@ -2,6 +2,31 @@ $(document).ready(function() {
 
     var thermostat = new Thermostat()
 
-    $('#temperature').text(thermostat.temp)
+    $('#temperature').text(thermostat.temp);
+
+    $('#temperature-up').on('click', function(){
+      thermostat.up(1);
+      $('#temperature').text(thermostat.temp);
+    });
+
+    $('#temperature-down').on('click', function(){
+      thermostat.down(1);
+      $('#temperature').text(thermostat.temp);
+    })
+
+    $('#temperature-reset').on('click', function(){
+      thermostat.reset();
+      $('#temperature').text(thermostat.temp);
+    })
+
+    $('#powersaving-off').on('click', function(){
+      thermostat.powerSavingMode = false;
+      $('#power-saving-status').text('off');
+    })
+
+    $('#powersaving-on').on('click', function(){
+      thermostat.powerSavingMode = true;
+      $('#power-saving-status').text('on');
+    })
 
 })
