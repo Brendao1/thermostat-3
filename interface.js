b869e54dbbcaf08eee6b88d9e774876534f7158b
“Brenda” <odonnell.brenda@gmail.com>
2019-04-21 08:51:58 +0100

    Removed unused code

--- interface.js
+++ interface.js
@@ -1,31 +1,37 @@
 $(document).ready(function() {
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temp);
+    $('#temperature').text(thermostat.temp + '\xB0');
     $('#temperature').attr('class', thermostat.energyUsage());
   };
 
+  function updateWeather() {
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=765b7effb4fe3023c06c61344f418207', function(data) {
+    $('#current-temperature').text(data.name + ": " + data.main.temp + '\xB0')
+  })
+  }
+
 
     var thermostat = new Thermostat()
+    updateTemperature()
+    updateWeather()
 
-    $('#temperature').text(thermostat.temp);
+    $('#temperature').text(thermostat.temp + '\xB0')
 
     $('#temperature-up').on('click', function(){
       thermostat.up(1);
-      // updateTemperature();
-      $('#temperature').text(thermostat.temp);
+      updateTemperature();
+      $('#temperature').text(thermostat.temp + '\xB0');
     });
 
     $('#temperature-down').on('click', function(){
       thermostat.down(1);
       updateTemperature();
-      // $('#temperature').text(thermostat.temp);
     })
 
     $('#temperature-reset').on('click', function(){
       thermostat.reset();
       updateTemperature();
-      // $('#temperature').text(thermostat.temp);
     })
 
     $('#powersaving-off').on('click', function(){
@@ -38,7 +44,7 @@ $(document).ready(function() {
       $('#power-saving-status').text('on');
     })
 
-    $('#energy-usage').text(thermostat.energyUsage());
+    //$('#energy-usage').text(thermostat.energyUsage());
 
 
 })
