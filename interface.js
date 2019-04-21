3d9d31f8ee9653914faea2c4fd241a8035ff517a
“Brenda” <odonnell.brenda@gmail.com>
2019-04-21 10:13:17 +0100

    Shows temperature for a chosen city from a list

--- interface.js
+++ interface.js
@@ -3,18 +3,34 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.temp + '\xB0');
     $('#temperature').attr('class', thermostat.energyUsage());
-  };
+  }
 
   function updateWeather() {
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=765b7effb4fe3023c06c61344f418207', function(data) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=London, uk&units=metric&appid=765b7effb4fe3023c06c61344f418207', function(data) {
     $('#current-temperature').text(data.name + ": " + data.main.temp + '\xB0')
-  })
+    })
+  }
+
+  function updateWeatherChosenCity(city) {
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric' + '&appid=765b7effb4fe3023c06c61344f418207', function(data) {
+    $('#city-temperature').text(data.name + ": " + data.main.temp + '\xB0')
+    })
   }
 
 
     var thermostat = new Thermostat()
     updateTemperature()
     updateWeather()
+    updateWeatherChosenCity($('#chosen-city').val())
+
+
+    $('#chosen-city').change(function() {
+      var city = $('#chosen-city').val()
+      // updateWeatherChosenCity(city)
+      $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric' + '&appid=765b7effb4fe3023c06c61344f418207', function(data) {
+      $('#city-temperature').text(data.name + ": " + data.main.temp + '\xB0')
+    })
+  })
 
     $('#temperature').text(thermostat.temp + '\xB0')
 
